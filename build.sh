0535d12d08b878695bb2fc8e4bb2d000ed652aba
neauoire <aliceffekt@gmail.com>
2023-08-02 08:34:07 -0700

    Updated drifloon

--- build.sh
+++ build.sh
@@ -1,17 +1,7 @@
 #!/bin/sh -e
 
-SOURCE="etc/hello.tal"
-TARGET="bin/output.rom"
-
-rm -rf bin
-mkdir bin
-
-# Assembler
-echo "Assembling formatter.."
+mkdir -p bin
 uxnasm etc/format-js.tal bin/format-js.rom
 uxnasm etc/drifloon.tal bin/drifloon.rom
-
-echo "Writing program.js.."
 uxncli bin/format-js.rom bin/drifloon.rom > src/asm.js
-echo "Done."
 
