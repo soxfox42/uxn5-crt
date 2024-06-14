18c3251c0907e0ff42961298e991ec39b1184db9
Devine Lu Linvega <aliceffekt@gmail.com>
2024-06-14 13:13:34 -0800

    Cleanup

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-mkdir -p bin
-uxnasm etc/format-js.tal bin/format-js.rom
-uxnasm etc/drifloon.tal bin/drifloon.rom
-# uxncli bin/format-js.rom bin/drifloon.rom > src/asm.js
-
-# uxnasm etc/palette.tal bin/res.rom
-# uxncli bin/format-js.rom bin/res.rom
