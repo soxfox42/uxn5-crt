ac0db50c33753656ee003e264e58a5a9aa3d3dc8
Devine Lu Linvega <aliceffekt@gmail.com>
2024-06-16 15:42:43 -0800

    Housekeeping

--- src/app.js
+++ src/app.js
@@ -1,30 +1,26 @@
 'use strict'
 
-let isEmbed = window.self !== window.top;
-
+const content_el = document.getElementById("content")
+const share_el = document.getElementById("share")
 const emulator = new Emu()
 
+/* Detect Embed */
+let isEmbed = window.self !== window.top;
 if (!isEmbed) {
 	document.body.className = "";
 }
 
-document.getElementById("content").style.display = "block"
+/* misc */
 
-emulator.bgCanvas = document.getElementById("bgcanvas");
-emulator.fgCanvas = document.getElementById("fgcanvas");
-emulator.screen.bgctx = emulator.bgCanvas.getContext("2d", {"willReadFrequently": true})
-emulator.screen.fgctx = emulator.fgCanvas.getContext("2d", {"willReadFrequently": true})
-emulator.screen.init()
-
-const share = new ShareView(document.getElementById("share"));
-const target_fps = 60;
+content_el.style.display = "block"
+const share = new ShareView(share_el);
 
 emulator.init().then(() => {
 	emulator.console.write_el = document.getElementById("console_std")
 	emulator.console.error_el = document.getElementById("console_err")
-	document.getElementById("screen").addEventListener("pointermove", emulator.pointer_moved)
-	document.getElementById("screen").addEventListener("pointerdown", emulator.pointer_down)
-	document.getElementById("screen").addEventListener("pointerup", emulator.pointer_up)
+	emulator.screen.el.addEventListener("pointermove", emulator.pointer_moved)
+	emulator.screen.el.addEventListener("pointerdown", emulator.pointer_down)
+	emulator.screen.el.addEventListener("pointerup", emulator.pointer_up)
 	window.addEventListener("keydown", emulator.controller.keyevent);
 	window.addEventListener("keyup", emulator.controller.keyevent);
 
@@ -47,7 +43,7 @@ emulator.init().then(() => {
 
 	setInterval(() => {
 		window.requestAnimationFrame(step);
-	}, 1000 / target_fps);
+	}, 1000 / 60);
 
 	if(!isEmbed) {
 		// Support dropping files
@@ -144,7 +140,7 @@ function ShareView(el) {
 		rom = v;
 		shareButtonEl.disabled = false;
 		popupEl.style.display = "none";
-		document.getElementById("share").style.display = "initial"
+		share_el.style.display = "initial"
 	}
 }
 
