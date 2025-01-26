9da8bf9ddeb305c1163b18b7dde9e518e6525a48
Devine Lu Linvega <aliceffekt@gmail.com>
2025-01-25 18:02:23 -0800

    Faster system expansion port

--- src/devices/system.js
+++ src/devices/system.js
@@ -4,9 +4,9 @@ function System(emu)
 {
 	this.expansion = (addr) => {
 		let operation = emu.uxn.ram[addr]
+		let length = peek16(emu.uxn.ram, addr + 1);
 		// fill
 		if(operation == 0){
-			let length = peek16(emu.uxn.ram, addr + 1);
 			let dst_page = peek16(emu.uxn.ram, addr + 3);
 			let dst_addr = peek16(emu.uxn.ram, addr + 5);
 			let value = emu.uxn.ram[addr + 7];
@@ -16,19 +16,13 @@ function System(emu)
 		}
 		// cpyl
 		else if(operation == 1){
-			let length = peek16(emu.uxn.ram, addr + 1);
-			let src_page = peek16(emu.uxn.ram, addr + 3);
-			let src_addr = peek16(emu.uxn.ram, addr + 5);
-			let dst_page = peek16(emu.uxn.ram, addr + 7);
-			let dst_addr = peek16(emu.uxn.ram, addr + 9);
-			let src = src_page * 0x10000;
-			let dst = dst_page * 0x10000;
-			for(let i = 0; i < length; i++)
-				emu.uxn.ram[dst + ((dst_addr + i) & 0xffff)] = emu.uxn.ram[src + ((src_addr + i) & 0xffff)];
+			let a = peek16(emu.uxn.ram, addr + 3) * 0x10000 + peek16(emu.uxn.ram, addr + 5);
+			let b = a + length;
+			let c = peek16(emu.uxn.ram, addr + 7) * 0x10000 + peek16(emu.uxn.ram, addr + 9);
+			for(; a < b; emu.uxn.ram[c++] = emu.uxn.ram[a++]);
 		}
 		// cpyr
 		else if(operation == 2){
-			let length = peek16(emu.uxn.ram, addr + 1);
 			let src_page = peek16(emu.uxn.ram, addr + 3);
 			let src_addr = peek16(emu.uxn.ram, addr + 5);
 			let dst_page = peek16(emu.uxn.ram, addr + 7);
